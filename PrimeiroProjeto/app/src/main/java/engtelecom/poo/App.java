b0ab1f6d79c4540bd4ad641d81d57f94d8da0ced
Gustavo Paulo <gustavo.p07@aluno.ifsc.edu.br>
2023-03-02 15:11:52 -0300

    Aula 07

---
+++ PrimeiroProjeto/app/src/main/java/engtelecom/poo/App.java
@@ -0,0 +1,12 @@
+package engtelecom.poo;
+
+import barcode.CodigoDeBarra;
+
+public class App {
+
+
+
+    public static void main(String[] args) {
+        CodigoDeBarra.gerarCodigoDeBarra("123456", "saida.png");
+    }
+}
