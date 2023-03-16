adf3de1c3cab39ab8b39fe499adfa849d16271cf
Gustavo Paulo <gustavo.p07@aluno.ifsc.edu.br>
2023-03-16 15:24:45 -0300

    Aula 11

---
+++ Aula11/app/src/main/java/engtelecom/poo/App.java
@@ -0,0 +1,33 @@
+package engtelecom.poo;
+
+import org.fusesource.jansi.AnsiConsole;
+import static org.fusesource.jansi.Ansi.*;
+import static org.fusesource.jansi.Ansi.Color.*;
+
+public class App {
+
+
+    public static void main(String[] args) throws Exception {
+         Barra a = new Barra(20, WHITE, '.', RED, '$', true);
+         a.iniciar();
+
+
+        // Exemplo do professor
+        /*  
+        AnsiConsole.systemInstall();
+        System.out.print(ansi().reset().eraseScreen().cursor(1,1));
+        System.out.print(ansi().fg(BLUE).a("[--------------------]"));
+        // System.out.println(ansi().fg(RED).a("G").fg(GREEN).a("u").fg(YELLOW).a("s").fg(BLUE).a("t").fg(MAGENTA).a("a").fg(CYAN).a("v").fg(RED).a("o"));
+
+        for (int i=2; i<22; i++){
+            
+            System.out.print(ansi().reset().cursor(1,i).fg(RED).a("#"));
+            
+            Thread.sleep(100); // dormindo 100ms
+        }
+
+        System.out.println();
+        AnsiConsole.systemUninstall();
+        */
+    }
+}
---
+++ Aula11/app/src/main/java/engtelecom/poo/Barra.java
@@ -0,0 +1,87 @@
+package engtelecom.poo;
+
+import org.fusesource.jansi.AnsiConsole;
+import static org.fusesource.jansi.Ansi.*;
+import static org.fusesource.jansi.Ansi.Color.*;
+
+import org.fusesource.jansi.Ansi.Color;
+
+public class Barra {
+    // Atributos
+
+    private int tamanhoBarra;
+    private Color corDimensao;
+    private char charDimensao;
+    private Color corProgresso;
+    private char charProgresso;
+    private boolean mostraPorcentagem = false;
+
+
+    /**
+     * Construtor
+     * @param tamanhoBarra
+     * @param corDimensao
+     * @param charDimensao
+     * @param corProgresso
+     * @param charProgresso
+     */
+
+    public Barra(int tamanhoBarra, Color corDimensao, char charDimensao, Color corProgresso, char charProgresso) {
+        if (tamanhoBarra >= 20 && tamanhoBarra <= 100){
+            this.tamanhoBarra = tamanhoBarra;
+            this.corDimensao = corDimensao;
+            this.charDimensao = charDimensao;
+            this.corProgresso = corProgresso;
+            this.charProgresso = charProgresso;
+        } 
+    }
+
+    public Barra(int tamanhoBarra, Color corDimensao, char charDimensao, Color corProgresso, char charProgresso, boolean porcentagem) {
+        if (tamanhoBarra >= 20 && tamanhoBarra <= 100){
+            this.tamanhoBarra = tamanhoBarra;
+            this.corDimensao = corDimensao;
+            this.charDimensao = charDimensao;
+            this.corProgresso = corProgresso;
+            this.charProgresso = charProgresso;
+            if (porcentagem == true){
+                this.mostraPorcentagem = porcentagem;
+            }
+        } 
+    }
+
+
+
+
+    /**
+     * Método que cria e preenche a barra de progresso conforme os atributos da classe Barra
+     * @throws Exception
+     */
+    public void iniciar() throws Exception{
+        AnsiConsole.systemInstall();
+        System.out.print(ansi().reset().eraseScreen().cursor(1,1));
+        for (int i=1; i<tamanhoBarra+1; i++){
+            System.out.print(ansi().fg(this.corDimensao).a(this.charDimensao));
+        }
+
+        for (int i=0; i<this.tamanhoBarra+1; i++){
+            System.out.print(ansi().reset().cursor(1,i).fg(this.corProgresso).a(this.charProgresso));
+            Thread.sleep(100);
+            if (mostraPorcentagem == true){
+                int tamanho = tamanhoBarra - i;
+                for (int j=0; j<tamanho; j++){
+                System.out.print(" ");
+                }
+                System.out.print(i + "/" + tamanhoBarra);
+            }
+        }
+
+        System.out.println();
+        AnsiConsole.systemUninstall();
+}
+
+    }
+
+
+
+    
+
