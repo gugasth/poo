c257d918d0f297ab1d8ef77922869db31dfab1eb
Gustavo Paulo <gustavo.p07@aluno.ifsc.edu.br>
2023-04-05 17:22:34 -0300

    Atualizações termometro

--- Aula14/app/src/main/java/engtelecom/poo/Termometro.java
+++ Aula14/app/src/main/java/engtelecom/poo/Termometro.java
@@ -4,17 +4,33 @@ public class Termometro {
     private int temperaturaAtual;
     private int temperaturaMinima;
     private int temperaturaMaxima;
-
+    private char escala;
 
     // Construtor
-    public Termometro(int temperaturaAtual, int temperaturaMinima, int temperaturaMaxima, String escala) {
+    public Termometro(int temperaturaAtual, int temperaturaMinima, int temperaturaMaxima, char escala) {
         this.temperaturaAtual = temperaturaAtual;
         this.temperaturaMinima = temperaturaMinima;
         this.temperaturaMaxima = temperaturaMaxima;
+
+        // Se a escala não for válida, então será Celsius por padrão.
+        if (escala == 'C' || escala == 'F' || escala == 'K'){
+            this.escala = escala;
+        } else {
+            this.escala =  'C';
+        }
     }
 
-    // Métodos
+
     public int getCelsius(){
+        if (this.escala == 'C'){
+            
+        } 
+        else if (this.escala == 'F'){
+
+        }
+        else if (this.escala == 'K'){
+
+        }
         return -1;
     }
 
@@ -26,10 +42,23 @@ public class Termometro {
         return -1;
     }
 
-    public int diferenca(Termometro t, String escala){
-        return -1;
+    /**
+     * Retorna a diferença de temperatura na escala informada
+     * @param t objeto da classe Termometro
+     * @param escala termométrica
+     * @return a diferença
+     */
+    public int diferenca(Termometro t, char escala){
+        if(escala == 'C'){
+            
+        }
+        if(escala == 'F'){
+            // implementar aqui
+        }
+        if(escala == 'K'){
+            // implementar aqui
+        }
+
+        return 0;
     }
-
-
-    
 }
--- Aula14/app/src/test/java/engtelecom/poo/TermometroTest.java
+++ Aula14/app/src/test/java/engtelecom/poo/TermometroTest.java
@@ -8,50 +8,79 @@ public class TermometroTest {
 
     @Test
     public void testaGetCelsius(){
-        Termometro tA = new Termometro(30, -50, 273, "C");
+        Termometro tA = new Termometro(30, -459, 373, 'C');
         assertEquals(30, tA.getCelsius());
 
-        Termometro tB = new Termometro(30, -50, 273, "F");
+        Termometro tB = new Termometro(30, -459, 373, 'F');
         assertEquals(-1, tB.getCelsius());
 
-        Termometro tC = new Termometro(30, -50, 273, "K");
+        Termometro tC = new Termometro(30, -459, 373, 'K');
         assertEquals(-243, tC.getCelsius());
     }
 
+
+
     @Test
     public void testaGetFahrenheit(){
-        Termometro tA = new Termometro(30, -50, 273, "C");
+        Termometro tA = new Termometro(30, -459, 373, 'C');
         assertEquals(86, tA.getFahrenheit());
 
-        Termometro tB = new Termometro(30, -50, 273, "F");
+        Termometro tB = new Termometro(30, -459, 373, 'F');
         assertEquals(30, tB.getFahrenheit());
 
-        Termometro tC = new Termometro(30, -50, 273, "K");
-        assertEquals(273, tC.getFahrenheit());
+        Termometro tC = new Termometro(30, -459, 373, 'K');
+        assertEquals(373, tC.getFahrenheit());
     }
 
     @Test
     public void testaGetKelvin(){
-        Termometro tA = new Termometro(30, -50, 273, "C");
+        Termometro tA = new Termometro(30, -459, 373, 'C');
         assertEquals(303, tA.getKelvin());
 
-        Termometro tB = new Termometro(30, -50, 273, "F");
+        Termometro tB = new Termometro(30, -459, 373, 'F');
         assertEquals(272, tB.getKelvin());
 
-        Termometro tC = new Termometro(30, -50, 273, "K");
+        Termometro tC = new Termometro(30, -459, 373, 'K');
         assertEquals(30, tC.getKelvin());
     }
     
 
     @Test
     public void testaDiferenca(){
-        Termometro tA = new Termometro(2, -50, 273, "C");
-        Termometro tB = new Termometro(2, -50, 273, "C");
+        Termometro tA = new Termometro(50, -459, 373, 'C');
+        Termometro tB = new Termometro(20, -459, 373, 'C');
+        assertEquals(30, tA.diferenca(tB, 'C'));
+        assertEquals(86, tA.diferenca(tB, 'F'));
+        assertEquals(303, tA.diferenca(tB, 'K'));
 
+        Termometro tC = new Termometro(-100, -459, 373, 'F');
+        Termometro tD = new Termometro(-200, -459, 373, 'F');
+        assertEquals(100, tC.diferenca(tD, 'F'));
+        assertEquals(38, tC.diferenca(tD, 'C'));
+        assertEquals(311, tC.diferenca(tD, 'K'));
 
+        Termometro tE = new Termometro(214, -459, 373, 'K');
+        Termometro tF = new Termometro(114, -459, 373, 'K');
+        assertEquals(100, tE.diferenca(tF, 'K'));
+        assertEquals(-173, tE.diferenca(tF, 'C'));
+        assertEquals(-280, tE.diferenca(tF, 'F'));
+
+        Termometro tG = new Termometro(25, -459, 373, 'C');
+        Termometro tH = new Termometro(50, -459, 373, 'F'); //50°F é 10°C
+        assertEquals(15, tG.diferenca(tH, 'C'));
+        assertEquals(59, tG.diferenca(tH, 'F'));
+        assertEquals(288, tG.diferenca(tH, 'K'));
+
+        Termometro tI = new Termometro(57, -459, 373, 'F');
+        Termometro tJ = new Termometro(276, -459, 373, 'K');
+        assertEquals(20, tI.diferenca(tJ, 'F'));
+        assertEquals(-7, tI.diferenca(tJ, 'C'));
+        assertEquals(266, tI.diferenca(tJ, 'K'));
+
+        Termometro tK = new Termometro(300, -459, 373, 'K');
+        Termometro tL = new Termometro(20, -459, 373, 'C');
+        assertEquals(7, tK.diferenca(tL, 'K'));
+        assertEquals(-266, tK.diferenca(tL, 'C'));
+        assertEquals(-447, tK.diferenca(tL, 'F'));
     }
-
-
-
-
 }
